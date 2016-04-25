b3045ec1aa94b0010ba21272be0a8e69e4fa6d84
hanzekivan <hanzekivan@eisti.eu>
2016-04-26 01:47:45 +0200

    FIX color black side

--- chess24.js
+++ chess24.js
@@ -1,28 +1,27 @@
 $(document).ready(function () {
     var port = chrome.runtime.connect({name: "easy-chess"});
-    var coords = [
-        'a8', 'a7', 'a6', 'a5', 'a4', 'a3', 'a2', 'a1',
-        'b8', 'b7', 'b6', 'b5', 'b4', 'b3', 'b2', 'b1',
-        'c8', 'c7', 'c6', 'c5', 'c4', 'c3', 'c2', 'c1',
-        'd8', 'd7', 'd6', 'd5', 'd4', 'd3', 'd2', 'd1',
-        'e8', 'e7', 'e6', 'e5', 'e4', 'e3', 'e2', 'e1',
-        'f8', 'f7', 'f6', 'f5', 'f4', 'f3', 'f2', 'f1',
-        'g8', 'g7', 'g6', 'g5', 'g4', 'g3', 'g2', 'g1',
-        'h8', 'h7', 'h6', 'h5', 'h4', 'h3', 'h2', 'h1'
-    ];
+    var coords = {
+        'a8': '70', 'a7': '60', 'a6': '50', 'a5': '40', 'a4': '30', 'a3': '20', 'a2': '10', 'a1': '00',
+        'b8': '71', 'b7': '61', 'b6': '51', 'b5': '41', 'b4': '31', 'b3': '21', 'b2': '11', 'b1': '01',
+        'c8': '72', 'c7': '62', 'c6': '52', 'c5': '42', 'c4': '32', 'c3': '22', 'c2': '12', 'c1': '02',
+        'd8': '73', 'd7': '63', 'd6': '53', 'd5': '43', 'd4': '33', 'd3': '23', 'd2': '13', 'd1': '03',
+        'e8': '74', 'e7': '64', 'e6': '54', 'e5': '44', 'e4': '34', 'e3': '24', 'e2': '14', 'e1': '04',
+        'f8': '75', 'f7': '65', 'f6': '55', 'f5': '45', 'f4': '35', 'f3': '25', 'f2': '15', 'f1': '05',
+        'g8': '76', 'g7': '66', 'g6': '56', 'g5': '46', 'g4': '36', 'g3': '26', 'g2': '16', 'g1': '06',
+        'h8': '77', 'h7': '67', 'h6': '57', 'h5': '47', 'h4': '37', 'h3': '27', 'h2': '17', 'h1': '07'
+    };
 
     port.onMessage.addListener(function (msg) {
-        var squares = ($('.chessGame').hasClass('w')) ? $('.cell').get() : $('.cell').get().reverse();
         var bestmove = JSON.parse(msg).bestmove;
+        var f = $('._' + coords[bestmove.from]);
+        var t = $('._' + coords[bestmove.to]);
 
-        var f = $(squares[coords.indexOf(bestmove.from)]);
-        var t = $(squares[coords.indexOf(bestmove.to)]);
-
-        $(squares).css('background-color', 'inherit');
+        $('.cell').each(function () {
+            $(this).css('background-color', 'inherit');
+        });
 
         f.css("background-color", "red");
         t.css("background-color", "red");
-        console.log(msg);
     });
 
     $('.current').on('DOMNodeInserted', function (e) {
