4448c9fd06b81848929132b9a7c599c633658a78
Ivan HANZEK <ivan.hanzek.hr@gmail.com>
2016-08-06 19:46:28 +0200

    jquery in login

--- js/popup.js
+++ js/popup.js
@@ -1,19 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var links = document.getElementsByTagName("a");
-    var login = document.getElementsByName("Submit");
+$(document).ready(function () {
+    $('a').each(function () {
+        $(this).on('click', function () {
+            chrome.tabs.create({active: true, url: $(this).attr('href')});
+        });
+    });
 
-    for (var i = 0; i < links.length; i++) {
-        (function () {
-            var ln = links[i];
-            var location = ln.href;
-            ln.onclick = function () {
-                chrome.tabs.create({active: true, url: location});
-            };
-        })();
-    }
-
-    login[0].onclick = function () {
-        
-    }
+    $('#submit').on('click', function () {
+        alert('fdsfssd');
+    });
 });
 
