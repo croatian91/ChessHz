c0611c5edbfff98db2c927f513d03e9cd8df5859
Ivan HANZEK <ivan.hanzek.hr@gmail.com>
2016-08-09 03:07:43 +0200

    login working!

--- js/popup.js
+++ js/popup.js
@@ -5,23 +5,44 @@ $(document).ready(function () {
         });
     });
 
-    $('#submit').on('click', function () {
-        var data = {
-            username: $('#username').val(),
-            password: $('#password').val()
+    $('#submit').on('click', function (e) {
+        var name = $('#username').val();
+        var pw = $('#password').val();
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://localhost:3000/auth",
+            "method": "POST",
+            "headers": {
+                "jsonp": false,
+                "name": name,
+                "password": pw,
+                "cache-control": "no-cache",
+                "content-type": "application/x-www-form-urlencoded"
+            },
+            "data": {
+                "name": name,
+                "password": pw
+            },
+            success: function (data, textStatus, jqXHR) {
+                if (data.success === true) {
+                    location.href = 'settings.html';
+                    chrome.browserAction.setPopup({
+                        popup: "settings.html"
+                    });
+                } else {
+                    $('#alert-msg').text(data.msg);
+                    $('.alert').css('display', 'block');
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $('#alert-msg').text(errorThrown);
+                $('.alert').css('display', 'block');
+            }
         };
 
-        $.ajax({
-            type: "POST",
-            url: "https://www.chesshz.top/login",
-            data: JSON.stringify(data),
-            success: function (res) {
-                console.log(res);
-            },
-            error: function (xhr, status) {
-                $(".alert").css("display", "block");
-            }
-        });
+        e.preventDefault();
+
+        $.ajax(settings);
     });
 });
-
