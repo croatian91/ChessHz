d6e3c9eef9e6586ba752f92023e4a77547faa006
Ivan HANZEK <ivan.hanzek.hr@gmail.com>
2016-08-10 05:28:25 +0200

    saves settings

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
-    $("[type='checkbox']").bootstrapSwitch('size', 'mini');
     (function ($) {
         var settings = $("[type='checkbox']");
+
+        $("[type='checkbox']").bootstrapSwitch('size', 'mini');
 
         $.each(settings, function (index, value) {
             var setting = $(value).attr('name');
@@ -10,13 +11,13 @@ $(document).ready(function () {
                 $("[name=" + setting).bootstrapSwitch('state', item[setting], true);
             });
         });
+
+        $("[type='checkbox']").on('switchChange.bootstrapSwitch', function (event, state) {
+            var setting = $(this).attr('name');
+            var o = {};
+
+            o[setting] = state;
+            chrome.storage.sync.set(o);
+        });
     })(jQuery);
-
-    $("[type='checkbox']").on('switchChange.bootstrapSwitch', function (event, state) {
-        var setting = $(this).attr('name');
-        var o = {};
-
-        o[setting] = state;
-        chrome.storage.sync.set(o);
-    });
 });
