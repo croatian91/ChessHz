2b4a0e60a49fc85f94c1d214ece6ce6878424544
Ivan HANZEK <ivan.hanzek.hr@gmail.com>
2016-08-10 05:24:55 +0200

    saves settings

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
     $("[type='checkbox']").bootstrapSwitch('size', 'mini');
+    (function ($) {
+        var settings = $("[type='checkbox']");
+
+        $.each(settings, function (index, value) {
+            var setting = $(value).attr('name');
+
+            chrome.storage.sync.get(setting, function (item) {
+                $("[name=" + setting).bootstrapSwitch('state', item[setting], true);
+            });
+        });
+    })(jQuery);
+
+    $("[type='checkbox']").on('switchChange.bootstrapSwitch', function (event, state) {
+        var setting = $(this).attr('name');
+        var o = {};
+
+        o[setting] = state;
+        chrome.storage.sync.set(o);
+    });
 });
