e8a4d12bc953a8deb8c1740e2037413672e0e6da
Ivan HANZEK <ivan.hanzek.hr@gmail.com>
2016-10-24 02:57:47 +0200

    gauge support added

--- js/chess.com.js
+++ js/chess.com.js
@@ -4,18 +4,61 @@
 
 $(document).ready(function () {
     var port = chrome.runtime.connect({name: "easy-chess"});
-
-    port.onMessage.addListener(function (data) {
-        var msg = JSON.parse(data);
-        var best = (msg.best !== undefined) ? msg.best.bestmove.from + msg.best.bestmove.to : 'none';
-        var ponder = (msg.best !== undefined) ? msg.best.ponder.from + msg.best.ponder.to : 'none';
-
-        console.log(msg);
-        $('span#ChessHz-message').text('Best move: ' + best + ' - ponder: ' + ponder);
-    });
+    var gauge = null;
+    const MIN = -10.0;
+    const MAX = 10.0;
 
     $.get(chrome.extension.getURL('/status.html'), function (data) {
-        $('#LiveChessMainContainer').prepend(data);
+        $('#LiveChessMainContainer').after(data);
+
+        gauge = new JustGage({
+            id: "gauge",
+            value: 0.0,
+            min: MIN,
+            max: MAX,
+            reverse: true,
+            title: "Evaluation",
+            gaugeColor: '#FFFFFF',
+            levelColors: ['#000000'],
+            refreshAnimationTime: 150,
+            decimals: 2
+        });
+
+        gauge.txtMin.attr({
+            "text": MIN
+        });
+
+        gauge.txtMax.attr({
+            "text": MAX
+        });
+    });
+
+    port.onMessage.addListener(function (data) {
+        var response = JSON.parse(data);
+
+        console.log(response);
+
+        if (response &&
+            response.hasOwnProperty('info') &&
+            response.hasOwnProperty('turn') &&
+            response.info.length > 0) {
+            var turn = (response.turn === 'w') ? 1 : -1;
+            var type = response.info[0].score.type;
+            var val = (type === 'cp') ? response.info[0].score.value * turn / 100 : ((turn > 0) ? MAX : MIN);
+
+            gauge.refresh(val);
+
+            if (type === 'mate')
+                gauge.txtValue.attr({
+                    "text": response.info[0].score.value
+                });
+
+            gauge.txtLabel.attr({
+                "text": type
+            });
+
+            $('span#ChessHz-message').text('Best move: ' + response.info[0].pv[0]);
+        }
     });
 
     $('#LiveChessTopSideBarTabset .tab-content').on('DOMNodeInserted', function (e) {
