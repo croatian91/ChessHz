7a7f46e4aa42747c7e8db1cf8f5c27f9f8fb8c81
Ivan HANZEK <ivan.hanzek.hr@gmail.com>
2016-10-26 17:47:21 +0200

    set options automatically on new connection

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 var ws = new WebSocket("ws://localhost:1337/");
 
 chrome.extension.onConnect.addListener(function (port) {
+    chrome.storage.sync.get('strength-slider', function (item) {
+        ws.send(JSON.stringify({
+            job: 'setOption',
+            setting: 'Skill Level',
+            value: item['strength-slider']
+        }));
+    });
+
     port.onMessage.addListener(function (request) {
         ws.send(request);
     });
